987598f65030547701026f50c238bf32ff173594
Tecno Espectro <TecnoEspectro15@hotmail.com>
2024-05-06 12:01:39 -0500

    Basic

---
+++ Archivos2/src/main/java/com/mycompany/archivos2/Archivos2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.archivos2;
+
+/**
+ *
+ * @author SCIS2-13
+ */
+public class Archivos2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
